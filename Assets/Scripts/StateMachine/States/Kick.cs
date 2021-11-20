e579450867e646bf2e015740d9b1511eebd94681
Tyler <tokurenov.amgalan@mail.ru>
2021-11-20 11:08:16 +0800

    test commit day2

--- Assets/Scripts/StateMachine/States/Kick.cs
+++ Assets/Scripts/StateMachine/States/Kick.cs
@@ -28,7 +28,18 @@ public class Kick : State
     {
         if (!Player.IsAttacking)
         {
-            StateMachine.ChangeState(StateMachine.PlayerStates.Idle);
+            if (Player.MovementSpeed < 0.1)
+            {
+                StateMachine.ChangeState(StateMachine.PlayerStates.Idle);
+            }
+            else if (Animator.GetFloat("Forward") > 0.1)
+            {
+                StateMachine.ChangeState(StateMachine.PlayerStates.RunForward);
+            }
+            else if (Animator.GetFloat("Backward") > 0.1)
+            {
+                StateMachine.ChangeState(StateMachine.PlayerStates.RunBackward);
+            }
         }
     }
 }
