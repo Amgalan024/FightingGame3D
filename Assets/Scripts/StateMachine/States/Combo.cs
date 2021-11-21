c146cf57b6b91cf65e7d3355f61777d5d1dcde33
Tyler <tokurenov.amgalan@mail.ru>
2021-11-21 10:36:17 +0800

    added ComboState

---
+++ Assets/Scripts/StateMachine/States/Combo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Combo : AttackState
+{
+    public string ComboName { set; get; }
+    public int ComboDamage { set; get; }
+
+    public Combo(Player player, StateMachine stateMachine, Animator animator, Rigidbody rigidbody, PlayerControls playerControls) : base(player, stateMachine, animator, rigidbody, playerControls)
+    {
+    }
+
+    public override void Enter()
+    {
+        Animator.Play(ComboName);
+        Animator.SetBool(ComboName,true);
+        Player.IsAttacking = true;
+        Player.SetDamage(ComboDamage);
+    }
+
+    public override void Exit()
+    {
+        Animator.SetBool(ComboName, false);
+    }
+
+    public override void FixedUpdate()
+    {
+        ExitAttackState();
+    }
+
+    public override void Update()
+    {
+    }
+}
+
