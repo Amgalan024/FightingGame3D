d3395f54c1c289f49c245ab1c806b66e03da4b9d
Tokurenov Amgalan <tokurenov.a@amayasoft.uz>
2023-01-06 07:47:23 +0800

    added player interactions controller

---
+++ Assets/MVC Pattern/Scripts/Gameplay/Controllers/PlayerControllers/PlayerInteractionsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using MVC.Gameplay.Models;
+using MVC.Gameplay.Models.Player;
+using MVC.StateMachine.States;
+using MVC.Views;
+using MVC_Pattern.Scripts.Gameplay.Services.StateMachine;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace MVC.Controllers
+{
+    public class PlayerInteractionsController : IInitializable, IDisposable
+    {
+        private readonly FightSceneModel _fightSceneModel;
+        private readonly IStateMachine _stateMachine;
+
+        private readonly PlayerContainer _playerContainer;
+        private readonly PlayerView _playerView;
+        private readonly PlayerModel _playerModel;
+        private readonly PlayerContainer _opponentContainer;
+
+        private readonly Transform _parent;
+
+        public PlayerInteractionsController(PlayerContainer playerContainer, IStateMachine stateMachine,
+            FightSceneModel fightSceneModel, LifetimeScope lifetimeScope)
+        {
+            _playerContainer = playerContainer;
+            _stateMachine = stateMachine;
+            _fightSceneModel = fightSceneModel;
+            _parent = lifetimeScope.transform;
+
+            _playerView = playerContainer.View;
+            _playerModel = playerContainer.Model;
+            _opponentContainer = playerContainer.OpponentContainer;
+        }
+
+        void IInitializable.Initialize()
+        {
+            _fightSceneModel.InvokePlayerSideCheck(_playerContainer);
+
+            _playerView.SetParent(_parent);
+
+            HandlePlayerEvents();
+        }
+
+        void IDisposable.Dispose()
+        {
+            DisposePlayerEvents();
+        }
+
+        private void HandlePlayerEvents()
+        {
+            _playerView.OnAttackAnimationEnded += OnAttackAnimationEnded;
+
+            _playerView.MainTriggerDetector.OnTriggerEntered += HandleBlock;
+
+            _playerView.CollisionDetector.OnCollisionEntered += OnCollisionEntered;
+            _playerView.CollisionDetector.OnCollisionExited += OnCollisionExit;
+
+            _playerView.SideDetectorView.OnTriggerEntered += InvokePlayerSideCheck;
+            _playerView.SideDetectorView.OnTriggerExited += InvokePlayerSideCheck;
+        }
+
+        private void DisposePlayerEvents()
+        {
+            _playerView.OnAttackAnimationEnded -= OnAttackAnimationEnded;
+
+            _playerView.MainTriggerDetector.OnTriggerEntered -= HandleBlock;
+
+            _playerView.CollisionDetector.OnCollisionEntered -= OnCollisionEntered;
+            _playerView.CollisionDetector.OnCollisionExited -= OnCollisionExit;
+
+            _playerView.SideDetectorView.OnTriggerEntered -= InvokePlayerSideCheck;
+            _playerView.SideDetectorView.OnTriggerExited -= InvokePlayerSideCheck;
+        }
+        
+        private void OnAttackAnimationEnded()
+        {
+            _playerModel.IsAttacking.Value = false;
+        }
+
+        private void HandleBlock(Collider collider)
+        {
+            if (collider.TryGetComponent(out TriggerDetectorView attackHitBox) &&
+                attackHitBox == _opponentContainer.AttackHitBox)
+            {
+                if (_playerModel.IsBlocking)
+                {
+                    _stateMachine.ChangeState<BlockState>();
+                }
+                else
+                {
+                    _playerModel.InvokePlayerAttacked(attackHitBox);
+                    _stateMachine.ChangeState<StunnedState>();
+                }
+            }
+        }
+
+        private void InvokePlayerSideCheck(Collider collider)
+        {
+            if (collider.GetComponent<PlayerView>())
+            {
+                _fightSceneModel.InvokePlayerSideCheck(_playerContainer);
+            }
+        }
+
+        private void OnCollisionEntered(Collision collision)
+        {
+            if (collision.gameObject.GetComponent<PlatformView>())
+            {
+                _playerModel.IsGrounded.Value = true;
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.GetComponent<PlatformView>())
+            {
+                _playerModel.IsGrounded.Value = false;
+            }
+        }
+    }
+}
--- Assets/MVC Pattern/Scripts/Gameplay/Controllers/PlayerControllers/PlayerStatesController.cs
+++ Assets/MVC Pattern/Scripts/Gameplay/Controllers/PlayerControllers/PlayerStatesController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks.Linq;
 using MVC.Configs.Enums;
 using MVC.Gameplay.Constants;
-using MVC.Gameplay.Models;
 using MVC.Gameplay.Models.Player;
 using MVC.Models;
 using MVC.StateMachine.States;
 using MVC.Views;
 using MVC_Pattern.Scripts.Gameplay.Models.StateMachineModels.StateModels;
 using MVC_Pattern.Scripts.Gameplay.Services.StateMachine;
-using UnityEngine;
 using VContainer.Unity;
 
 namespace MVC.Controllers
@@ -19,40 +17,27 @@ namespace MVC.Controllers
     {
         private readonly IStateMachine _stateMachine;
         private readonly InputActionModelsContainer _inputActionModelsContainer;
-        private readonly FightSceneModel _fightSceneModel;
         private readonly PlayerModel _playerModel;
         private readonly PlayerView _playerView;
-        private readonly PlayerContainer _opponentContainer;
-        private readonly PlayerContainer _playerContainer;
         private readonly InputModelsContainer _inputModelsContainer;
-
-        private readonly Transform _parent;
 
         private readonly List<IDisposable> _subscriptions = new List<IDisposable>(5);
 
         private readonly RunStateModel _runStateModel;
 
         public PlayerStatesController(PlayerContainer playerContainer, IStateMachine stateMachine,
-            FightSceneModel fightSceneModel, LifetimeScope lifetimeScope, RunStateModel runStateModel)
+            RunStateModel runStateModel)
         {
-            _playerContainer = playerContainer;
             _playerModel = playerContainer.Model;
             _playerView = playerContainer.View;
             _inputModelsContainer = playerContainer.InputModelsContainer;
             _inputActionModelsContainer = playerContainer.InputActionModelsContainer;
-            _opponentContainer = playerContainer.OpponentContainer;
             _stateMachine = stateMachine;
-            _fightSceneModel = fightSceneModel;
             _runStateModel = runStateModel;
-            _parent = lifetimeScope.transform;
         }
 
         void IStartable.Start()
         {
-            _fightSceneModel.InvokePlayerSideCheck(_playerContainer);
-
-            _playerView.SetParent(_parent);
-
             _stateMachine.ChangeState<IdleState>();
 
             HandleInputEvents();
@@ -91,16 +76,6 @@ namespace MVC.Controllers
 
         private void HandlePlayerEvents()
         {
-            _playerView.OnAttackAnimationEnded += OnAttackAnimationEnded;
-
-            _playerView.MainTriggerDetector.OnTriggerEntered += HandleBlock;
-
-            _playerView.CollisionDetector.OnCollisionEntered += OnCollisionEntered;
-            _playerView.CollisionDetector.OnCollisionExited += OnCollisionExit;
-
-            _playerView.SideDetectorView.OnTriggerEntered += InvokePlayerSideCheck;
-            _playerView.SideDetectorView.OnTriggerExited += InvokePlayerSideCheck;
-
             _playerModel.OnWin += _stateMachine.ChangeState<WinState>;
             _playerModel.OnLose += _stateMachine.ChangeState<LoseState>;
 
@@ -114,71 +89,14 @@ namespace MVC.Controllers
                 _playerView.Animator.SetBool(PlayerAnimatorData.IsCrouching, isCrouching)));
         }
 
-
         private void DisposePlayerEvents()
         {
-            _playerView.OnAttackAnimationEnded -= OnAttackAnimationEnded;
-
-            _playerView.MainTriggerDetector.OnTriggerEntered -= HandleBlock;
-
-            _playerView.CollisionDetector.OnCollisionEntered -= OnCollisionEntered;
-            _playerView.CollisionDetector.OnCollisionExited -= OnCollisionExit;
-
-            _playerView.SideDetectorView.OnTriggerEntered -= InvokePlayerSideCheck;
-            _playerView.SideDetectorView.OnTriggerExited -= InvokePlayerSideCheck;
-
             _playerModel.OnWin -= _stateMachine.ChangeState<WinState>;
             _playerModel.OnLose -= _stateMachine.ChangeState<LoseState>;
 
             foreach (var subscription in _subscriptions)
             {
                 subscription.Dispose();
-            }
-        }
-
-        private void OnAttackAnimationEnded()
-        {
-            _playerModel.IsAttacking.Value = false;
-        }
-
-        private void HandleBlock(Collider collider)
-        {
-            if (collider.TryGetComponent(out TriggerDetectorView attackHitBox) &&
-                attackHitBox == _opponentContainer.AttackHitBox)
-            {
-                if (_playerModel.IsBlocking)
-                {
-                    _stateMachine.ChangeState<BlockState>();
-                }
-                else
-                {
-                    _playerModel.InvokePlayerAttacked(attackHitBox);
-                    _stateMachine.ChangeState<StunnedState>();
-                }
-            }
-        }
-
-        private void InvokePlayerSideCheck(Collider collider)
-        {
-            if (collider.GetComponent<PlayerView>())
-            {
-                _fightSceneModel.InvokePlayerSideCheck(_playerContainer);
-            }
-        }
-
-        private void OnCollisionExit(Collision collision)
-        {
-            if (collision.gameObject.GetComponent<PlatformView>())
-            {
-                _playerModel.IsGrounded.Value = false;
-            }
-        }
-
-        private void OnCollisionEntered(Collision collision)
-        {
-            if (collision.gameObject.GetComponent<PlatformView>())
-            {
-                _playerModel.IsGrounded.Value = true;
             }
         }
 
