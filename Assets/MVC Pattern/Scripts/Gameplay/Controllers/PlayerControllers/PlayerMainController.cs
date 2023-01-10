b46f2bd35c9b1efcb45af27f9f383e8d4f803b71
Tokurenov Amgalan <tokurenov.a@amayasoft.uz>
2023-01-10 11:59:07 +0800

    fixed PlayerMainController

--- Assets/MVC Pattern/Scripts/Gameplay/Controllers/PlayerControllers/PlayerMainController.cs
+++ Assets/MVC Pattern/Scripts/Gameplay/Controllers/PlayerControllers/PlayerMainController.cs
@@ -1,26 +1,43 @@
-﻿using MVC.Gameplay.Models.Player;
+﻿using System;
+using MVC.Gameplay.Models.Player;
 using MVC.StateMachine.States;
+using MVC.Views;
+using UnityEngine;
 using VContainer.Unity;
 
 namespace MVC.Controllers
 {
-    public class PlayerMainController : IInitializable
+    public class PlayerMainController : IInitializable, IDisposable
     {
         private readonly PlayerModel _playerModel;
+        private readonly PlayerView _playerView;
         private readonly PlayerContainer _opponentContainer;
         private readonly StateMachine.StateMachine _stateMachine;
 
-        public PlayerMainController(PlayerContainer playerContainer, StateMachine.StateMachine stateMachine)
+        private readonly Transform _parent;
+
+        public PlayerMainController(PlayerContainer playerContainer, StateMachine.StateMachine stateMachine,
+            LifetimeScope lifetimeScope)
         {
             _stateMachine = stateMachine;
+            _parent = lifetimeScope.transform;
             _playerModel = playerContainer.Model;
+            _playerView = playerContainer.View;
             _opponentContainer = playerContainer.OpponentContainer;
         }
 
-        public void Initialize()
+        void IInitializable.Initialize()
         {
+            _playerView.SetParent(_parent);
+
             _playerModel.OnLose += HandleLose;
             _playerModel.OnWin += HandleWin;
+        }
+
+        void IDisposable.Dispose()
+        {
+            _playerModel.OnLose -= HandleLose;
+            _playerModel.OnWin -= HandleWin;
         }
 
         private void HandleLose()
