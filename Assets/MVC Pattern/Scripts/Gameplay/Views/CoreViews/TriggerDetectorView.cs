7c138de2c9e14bfe91077e982bccd476809572d7
Tokurenov Amgalan <tokurenov.a@amayasoft.uz>
2023-01-10 15:27:36 +0800

    fixed TriggerDetectorView

--- Assets/MVC Pattern/Scripts/Gameplay/Views/CoreViews/TriggerDetectorView.cs
+++ Assets/MVC Pattern/Scripts/Gameplay/Views/CoreViews/TriggerDetectorView.cs
@@ -6,13 +6,21 @@ namespace MVC.Views
     public class TriggerDetectorView : MonoBehaviour
     {
         public event Action<Collider> OnTriggerEntered;
+        public event Action<Collider> OnTriggerStaying;
         public event Action<Collider> OnTriggerExited;
 
+        [SerializeField] private Collider _collider;
 
+        public Collider Collider => _collider;
 
         public void OnTriggerEnter(Collider other)
         {
             OnTriggerEntered?.Invoke(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            OnTriggerStaying?.Invoke(other);
         }
 
         public void OnTriggerExit(Collider other)
